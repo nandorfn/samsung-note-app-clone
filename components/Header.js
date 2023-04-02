0cc5ef4e2587e50b6046b819be5e07f9b2c59e2c
nandorfn <firnandorfn@gmail.com>
2023-04-02 19:52:48 +0700

    add TailwindCSS

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+    return(
+      <>
+        <div className="text-center">
+          <h1>Semua Catatan</h1>
+          <p>17 Catatan</p>
+        </div>
+      </>  
+    );
+}
