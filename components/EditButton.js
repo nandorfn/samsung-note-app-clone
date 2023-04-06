7bb5bfd29655ab8eb7ceb1be69ea0b36355bac66
nandorfn <firnandorfn@gmail.com>
2023-04-06 20:59:18 +0700

    add important t btn

--- components/EditButton.js
+++ components/EditButton.js
@@ -6,7 +6,7 @@ export default function EditButton(props) {
     return (
         <div onClick={props.toggle} >
             <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <Fab className='fixed bottom-4 right-4 bg-[#171717]' color="warning" aria-label="add">
+                <Fab className='fixed bottom-4 right-4 bg-[#171717] !important' color="warning" aria-label="add">
                     <EditIcon className='absolute'/>
                 </Fab>
             </Box>
