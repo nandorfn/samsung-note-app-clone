03e825a8091400d5237a55a73dffd9cceb0f17a4
nandorfn <firnandorfn@gmail.com>
2023-04-06 21:05:52 +0700

    add important t btn

--- components/EditButton.js
+++ components/EditButton.js
@@ -3,11 +3,24 @@ import EditIcon from '@mui/icons-material/Edit';
 import Box from '@mui/material/Box';
 
 export default function EditButton(props) {
+    const fabStyle = {
+        position: 'fixed',
+        bottom: '4px',
+        right: '4px',
+        backgroundColor: '#171717',
+        color: 'white',
+        zIndex: 999,
+    };
+
+    const editIconStyle = {
+        position: 'absolute',
+    };
+
     return (
         <div onClick={props.toggle} >
             <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <Fab className='fixed bottom-4 right-4 bg-[#171717] !important' color="warning" aria-label="add">
-                    <EditIcon className='absolute'/>
+                <Fab style={fabStyle} color="warning" aria-label="add">
+                    <EditIcon style={editIconStyle} />
                 </Fab>
             </Box>
         </div>
