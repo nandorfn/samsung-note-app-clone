8335b2f4a2483e6607c254ad45b4e5917e2a2547
nandorfn <firnandorfn@gmail.com>
2023-04-06 21:08:38 +0700

    edit float btn

--- components/FloatingButton.js
+++ components/FloatingButton.js
@@ -1,24 +1,13 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
-import EditIcon from '@mui/icons-material/Edit';
 import CheckIcon from '@mui/icons-material/CheckCircleRounded';
-import Link from 'next/link';
 
-export default function FloatingActionButtonSize() {
-    return (
-        <Box sx={{ '& > :not(style)': { m: 1 } }}>
-            <Fab className='fixed bottom-4 right-4 bg-[#171717]' color="warning" aria-label="add">
-                <EditIcon />
-            </Fab>
-        </Box>
-    );
-}
 
 export function FloatingDoneButtonSize() {
     return (
         <Box sx={{ '& > :not(style)': { m: 1 } }}>
-            <Fab className='fixed bottom-4 right-4 bg-[#171717]' color="warning" aria-label="add">
+            <Fab sx={{position: 'fixed', bottom: '4rem', right: '4rem', bgcolor: '#171717 !important'}} color="warning" aria-label="add">
                 <CheckIcon />
             </Fab>
         </Box>
