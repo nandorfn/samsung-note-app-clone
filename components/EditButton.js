fd67ff49aa5796cff5494cecb3fb27457be4d37e
nandorfn <firnandorfn@gmail.com>
2023-04-06 20:38:43 +0700

    test

--- components/EditButton.js
+++ components/EditButton.js
@@ -1,12 +1,15 @@
 import Fab from '@mui/material/Fab';
 import EditIcon from '@mui/icons-material/Edit';
+import Box from '@mui/material/Box';
 
 export default function EditButton(props) {
     return (
         <div onClick={props.toggle} >
-            <Fab className='fixed bottom-4 right-4 bg-[#171717]' color="warning" aria-label="add">
-                <EditIcon position='absolute'/>
-            </Fab>
+            <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                <Fab className='fixed bottom-4 right-4 bg-[#171717]' color="warning" aria-label="add">
+                    <EditIcon />
+                </Fab>
+            </Box>
         </div>
     );
 }
